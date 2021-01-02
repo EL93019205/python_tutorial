fea681fb6062baeb87eae4014563eb925e4351a6
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-03 03:10:01 +0900

    数値、文字列、boolの出力とその型の出力

--- lesson.py
+++ lesson.py
@@ -1,5 +1,11 @@
-a = 'test'
-b = a
-c = b
-print(c)
+num = 1
+name = 'Mike'
+is_ok = True
 
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+# 1 <class 'int'>
+# Mike <class 'str'>
+# True <class 'bool'>
