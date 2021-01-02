7793e0e54ddada8d279f70e160d84dac16a755d0
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-03 03:12:15 +0900

    数値型変数に文字列変数を代入すると文字列変数に変わる

--- lesson.py
+++ lesson.py
@@ -1,11 +1,12 @@
 num = 1
 name = 'Mike'
+num = name
 is_ok = True
 
 print(num, type(num))
 print(name, type(name))
 print(is_ok, type(is_ok))
 
-# 1 <class 'int'>
+# Mike <class 'str'>
 # Mike <class 'str'>
 # True <class 'bool'>
