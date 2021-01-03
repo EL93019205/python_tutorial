933706053c747d167204c5e7913d3290560aa5e2
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-04 02:58:23 +0900

    タプルのアンパッキングを扱ってみよう

--- main.py
+++ main.py
@@ -276,8 +276,36 @@ print(new_tuple)
 new_tuple=(1,)+(4,5,6)
 print(new_tuple)
 
+print("""\
+########################################
+# タプルのアンパッキングを扱ってみよう #
+########################################\
+""")
+num_tuple = (10,20)
+print(num_tuple)
+x, y = num_tuple
+print(x,y)
 
+x, y = (10, 20)
+print(x,y)
 
+min, max = 0, 100
+print(min, max)
 
+a,b,c,d,e,f = 'Mike', 'l','l','l','e','f'
+a='Mike'
+b='1'
 
+i=10
+j=20
+tmp=i
+i=j
+j=tmp
+print(i,j)
+
+a=100
+b=200
+print(a,b)
+a,b=b,a
+print(a,b)
 
