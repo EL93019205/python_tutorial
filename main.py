b7934d0d5008fae2b398a47cb47176c1035ed228
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-04 02:48:16 +0900

    タプル型を扱ってみよう

--- main.py
+++ main.py
@@ -235,5 +235,49 @@ print(min <= len(seat) < max)
 seat.pop(0)
 print(min <= len(seat) < max)
 
+print("""\
+##########################
+# タプル型を扱ってみよう #
+##########################\
+""")
+t = (1,2,3,4,1,2)
+print(t)
+print(type(t))
+#t[0]=100
+print(t[0])
+print(t[2:5])
+print(t.index(1))
+print(t.index(1,1))
+print(t.count(1))
+print(t)
+print(t)
+print(t)
+t=([1,2,3],[4,5,6])
+t[0][0] = 100
+print(t)
+t = 1,2,3
+print(type(t))
+print(t)
+t=1,
+print(type(t))
+t=()
+print(type(t))
+t=(1)
+print(type(t))
+t=('test')
+print(type(t))
+t=('test',)
+print(type(t))
+t=1,
+#t+100
+new_tuple = (1,2,3)+(4,5,6)
+print(new_tuple)
+#new_tuple =(1)+(4,5,6)
+new_tuple=(1,)+(4,5,6)
+print(new_tuple)
+
+
+
+
 
 
