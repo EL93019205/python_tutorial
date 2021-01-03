e52308a9972a6076208c84aa5a83beb5c1a706bf
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-04 03:31:50 +0900

    集合型を扱ってみよう

--- main.py
+++ main.py
@@ -388,13 +388,41 @@ fruits = {
 
 print(fruits['apple'])
 
-
-
-
-
-
-
-
+print("""\
+########################
+# 集合型を扱ってみよう #
+########################\
+""")
+a = {1,2,2,3,4,4,4,5,6}
+print(a)
+print(type(a))
+b={2,3,3,6,7}
+print(b)
+print(a-b)
+print(b-a)
+print(a & b)
+print(a|b)
+print(a^b)
+s={1,2,3,4,5}
+print(s)
+#print(s[0])
+s.add(6)
+print(s)
+s.add(6)
+print(s)
+s.remove(6)
+print(s)
+s.clear()
+print(s)
+a={}
+print(type(a))
+print(a)
+my_friends = {'A', 'C', 'D'}
+A_friends = {'B','D','E','F'}
+print(my_friends & A_friends)
+f = ['apple', 'banana', 'apple', 'banana']
+kind = set(f)
+print(kind)
 
 
 
