b8ecc7932edbfd6dd1d49f93ca52f67259f123ca
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 03:24:09 +0900

    for,break,continueを扱おう

--- main.py
+++ main.py
@@ -581,16 +581,36 @@ print("""\
 # input関数を扱おう #
 #####################\
 """)
+"""
 while True:
 	word=input('Enter:')
 	if word=='ok':
 		break
 	print('next')
+"""
+print("""\
+##############################
+# for,break,continueを扱おう #
+##############################\
+""")
+some_list=[1,2,3,4,5]
+"""
+i=0
+while i < len(some_list):
+	print(some_list[i])
+	i+= 1
+"""
+for i in some_list:
+	print(i)
+for s in 'abcde':
+	print(s)
 
-
-
-
-
+for word in ['My', 'name', 'is', 'Mike']:
+	if word == 'name':
+		continue
+	if word == 'Mike':
+		break
+	print(word)
 
 
 
