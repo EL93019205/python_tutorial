815819350dfc12995e64b7a96515471cb9922d1b
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 03:54:15 +0900

    関数を定義しよう

--- main.py
+++ main.py
@@ -677,6 +677,37 @@ print(d.items())
 for k, v in d.items():
 	print(k, ':', v)
 
+print("""\
+####################
+# 関数を定義しよう #
+####################\
+""")
+def say_something():
+	print('hi')
+say_something()
+say_something
+print(type(say_something))
+f=say_something
+f()
+def say_something():
+	s='hi'
+	return s
+result=say_something()
+print(result)
 
+def what_is_this(color):
+	if color == 'red':
+		return 'tomato'
+	elif color == 'green':
+		return 'green pepper'
+	else:
+		return "I don't know"
+
+result = what_is_this('red')
+print(result)
+result = what_is_this('green')
+print(result)
+result = what_is_this('yellow')
+print(result)
 
 
