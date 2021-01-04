f5659deef65e2c432098b765687dd1d13cc1e530
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 02:33:38 +0900

    -m

--- main.py
+++ main.py
@@ -500,10 +500,28 @@ print(a>0 and b>0)
 # aまたはbが真であれば真
 print(a>0 or b>0)
 
-
-
-
-
+print("""\
+###################
+# InとNotを扱おう #
+###################\
+""")
+y=[1,2,3]
+x=1
+if x in y:
+	print('in')
+if 100 not in y:
+	print('not in')
+a=1
+b=2
+# notは奨められていない
+if not a == b:
+	print('not equal')
+# こっちが推奨
+if a != b:
+	print('not equal')
+is_ok=True
+if not is_ok:
+	print('hello')
 
 
 
