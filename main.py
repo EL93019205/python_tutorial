8c3c828131a7d3aa4021e394d5cfc02339bb5a66
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 02:16:36 +0900

    -m

--- main.py
+++ main.py
@@ -451,6 +451,29 @@ print(s)
 
 # 1行は80文字で収めるべきというルールが一応ある
 
+print("""\
+######################
+# if文を扱ってみよう #
+######################
+\
+""")
+x=10
+if x < 0:
+	print('negative')
+elif x == 0:
+	print('zero')
+elif x == 10:
+	print('100000')
+elif x == 10:
+	print('10')
+else:
+	print('positive')
 
+a=5
+b=10
+if a > 0:
+	print('a is positive')
+	if b > 0:
+		print('b is positive')
 
 
