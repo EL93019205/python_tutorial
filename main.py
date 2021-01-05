653e192c6b93cbba5183a0082dd75560e9a54e46
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 22:51:33 +0900

    クロージャーを扱ってみよう

--- main.py
+++ main.py
@@ -805,9 +805,9 @@ print(example_func.__doc__)
 help(example_func)
 
 print("""\
-##############
-# 関数内関数 #
-##############\
+############################
+# 関数内関数を扱ってみよう #
+############################\
 """)
 def outer(a,b):
 	def plus(c,d):
@@ -817,7 +817,32 @@ def outer(a,b):
 	print(r1+r2)
 outer(1,2)
 
+print("""\
+##############################
+# クロージャーを扱ってみよう #
+##############################\
+""")
+def outer(a,b):
+	def inner():
+		return a+b
+	return inner
 
+
+f=outer(1,2)
+r=f()
+print(r)
+
+def circle_area_func(pi):
+	def circle_area(radius):
+		return pi * radius * radius
+
+	return circle_area
+
+ca1=circle_area_func(3.14)
+ca2=circle_area_func(3.141592)
+
+print(ca1(10))
+print(ca2(10))
 
 
 
