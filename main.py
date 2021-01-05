64dafd5a0d881df676da94fbdcd5faba30412bc4
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-06 01:06:32 +0900

    独自例外を作成しよう

--- main.py
+++ main.py
@@ -1077,7 +1077,23 @@ else:
 finally:
 	print('clean up')
 
+print("""\
+########################
+# 独自例外を作成しよう #
+########################\
+""")
+class UppercaseError(Exception):
+	pass
 
+def check():
+	words = ['APPLE', 'orange', 'banana']
+	for word in words:
+		if word.isupper():
+			raise UppercaseError(word)
 
+try:
+	check()
+except UppercaseError as exc:
+	print('This is my fault. Go next')
 
 
