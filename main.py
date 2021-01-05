8eddc5662275bb1b19377ec0b9be265792bf0b67
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-06 00:26:04 +0900

    集合内包表記を扱おう

--- main.py
+++ main.py
@@ -993,4 +993,21 @@ print(d)
 d={x:y for x, y in zip(w,f)}
 print(d)
 
+print("""\
+########################
+# 集合内包表記を扱おう #
+########################\
+""")
+s = set()
+
+for i in range(10):
+	if i % 2 == 0:
+		s.add(i)
+
+print(s)
+
+s = {i for i in range(10) if i % 2 == 0}
+print(s)
+
+
 
