7514306279282350b643335e9fa3473f4ed2cb52
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 22:30:23 +0900

    キーワード引数を辞書化しよう

--- main.py
+++ main.py
@@ -771,4 +771,17 @@ def say_something(word,*args):
 t=('Mike','Nancy')
 say_something('Hi!' , *t)
 
+print("""\
+################################
+# キーワード引数を辞書化しよう #
+################################\
+""")
+def menu(food, *args, **kwargs):
+	print(food)
+	print(args)
+	print(kwargs)
+menu('banana', 'apple', 'orange', entree='beef', drink='coffee')
+
+
+
 
