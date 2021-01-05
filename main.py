d5ac1b31260bd6bee33aa752bc2f02569456fe8c
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 23:28:55 +0900

    ラムダを扱ってみよう

--- main.py
+++ main.py
@@ -890,4 +890,23 @@ print(r)
 #r=f(10,20)
 #print(r)
 
+print("""\
+########################
+# ラムダを扱ってみよう #
+########################\
+""")
+l=['Mon','tue','Wed','Thu','fri','sat','Sun']
+
+def change_words(words,func):
+	for word in words:
+		print(func(word))
+"""
+def sample_func(word):
+	return word.capitalize()
+"""
+change_words(l,lambda word: word.capitalize())
+change_words(l,lambda word: word.lower())
+
+
+
 
