15aaaa51def5fff548ba10b274717d51461d8553
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 22:42:50 +0900

    無駄なシェルスクリプトを削除

--- watch.sh
+++ watch.sh
@@ -1,1 +1,1 @@
-watch -t -d "./run.sh > result.txt 2> error.txt; cat ./result.txt | tail -n 30; cat error.txt"
+watch -t -d "python main.py > result.txt 2> error.txt; cat ./result.txt | tail -n 30; cat error.txt"
