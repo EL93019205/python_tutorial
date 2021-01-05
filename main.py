445b16e9c772d781a09e133b52573ce5f75f3fed
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-05 22:40:07 +0900

    関数内関数を扱おう

--- main.py
+++ main.py
@@ -804,9 +804,18 @@ def example_func(param1, param2):
 print(example_func.__doc__)
 help(example_func)
 
-
-
-
+print("""\
+##############
+# 関数内関数 #
+##############\
+""")
+def outer(a,b):
+	def plus(c,d):
+		return c+d
+	r1=plus(a,b)
+	r2=plus(b,a)
+	print(r1+r2)
+outer(1,2)
 
 
 
