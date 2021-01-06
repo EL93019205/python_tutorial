96cc5814bbe92f5b715bdb48ed2c43f2322cd65a
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-06 23:57:57 +0900

    ImportErrorを扱おう

--- main.py
+++ main.py
@@ -1144,7 +1144,12 @@ print("""\
 # ImportErrorを扱おう #
 #######################\
 """)
-utils.say_twice('word')
+try:
+	from lesson_package import utils
+except ImportError:
+	from lesson_package.tools import utils
+
+print(utils.say_twice('word'))
 
 
 
