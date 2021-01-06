598415e7e50d0539703aacb6bcd220cac22d3d04
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-07 00:29:16 +0900

    標準ライブラリを扱おう

--- main.py
+++ main.py
@@ -1166,12 +1166,31 @@ for key in ranking:
 	print(key)
 ranking.get('A')
 print(sorted(ranking,key=ranking.get,reverse=True))
+print("""\
+##########################
+# 標準ライブラリを扱おう #
+##########################\
+""")
+s = "fjaskl;jfljslkjzmxnfkhjaiueorqp,mbnzvhhiuahfena"
+d={}
+for c in s:
+	if c not in d:
+		d[c]=0
+	d[c]+=1
+print(d)
 
+d={}
+for c in s:
+	d.setdefault(c,0)
+	d[c] += 1
+print(d)
 
-
-
-
-
+from collections import defaultdict
+d=defaultdict(int)
+for c in s:
+	d[c] += 1
+print(d)
+print(d['f'])
 
 
 
