3e88383f88c70abadef54046597f90fb7c5a00b6
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-09 03:39:39 +0900

    CSVファイルへの書き込みと読み込みを扱おう

--- main.py
+++ main.py
@@ -1671,6 +1671,25 @@ with open('design/email_template.txt') as f:
 contents=t.substitute(name='Mike',contents='How are you?')
 print(contents)
 
+print("""\
+#############################################
+# CSVファイルへの書き込みと読み込みを扱おう #
+#############################################\
+""")
+import csv
+with open('test.csv', 'w') as csv_file:
+	fieldnames = ['Name', 'Count']
+	writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+	writer.writeheader()
+	writer.writerow({'Name': 'A', 'Count': 1})
+	writer.writerow({'Name': 'B', 'Count': 2})
+
+with open('test.csv', 'r')as csv_file:
+	reader = csv.DictReader(csv_file)
+	for row in reader:
+		print(row['Name'], row['Count'])
+
+
 
 
 
