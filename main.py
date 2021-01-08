01aa6b612c76d69d99256edb18a6b3b1a3567228
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-09 00:09:16 +0900

    プロパティーを使った属性の設定をしよう

--- main.py
+++ main.py
@@ -1369,6 +1369,51 @@ tesla_car = TeslaCar('Model S')
 print(tesla_car.model)
 tesla_car.run()
 tesla_car.auto_run()
+print("""\
+##########################################
+# プロパティーを使った属性の設定をしよう #
+##########################################\
+""")
+class Car(object):
+	def __init__(self, model=None):
+		self.model=model
+		# print(model+'aasdffafsgg')
+	def run(self):
+		print('run')
+
+class ToyotaCar(Car):
+	def run(self):
+		print('fast')
+
+class TeslaCar(Car):
+	def __init__(self, model='Model S', 
+				enable_auto_run=False,
+				passwd='123'):
+		# self.model=model
+		super().__init__(model)
+		self.__enable_auto_run=enable_auto_run
+		self.passwd = passwd
+	
+	@property
+	def enable_auto_run(self):
+		return self._enable_auto_run
+	
+	@enable_auto_run.setter
+	def enable_auto_run(self, is_enable):
+		if self.passwd == '456':
+			self._enable_auto_run=is_enable
+		else:
+			raise ValueError
+
+	def run(self):
+		print(self.__enable_auto_run)
+		print('super fast')
+	def auto_run(self):
+		print('auto run')
+tesla_car = TeslaCar('Model S',passwd="111")
+tesla_car.run()
+#print(tesla_car.__enable_auto_run)
+
 
 
 
