05c9d3301b60ff3d6b30ad516a868fb58f2228b5
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-09 02:32:07 +0900

    ファイルを読み込もう

--- main.py
+++ main.py
@@ -1644,9 +1644,27 @@ print("""\
 # withステートメントでファイルをオープンしよう #
 ################################################\
 """)
-with open('test.txt', 'w') as f:
-	f.write('test')
-
+print("""\
+########################
+# ファイルを読み込もう #
+########################\
+""")
+s="""\
+AAA
+BBB
+CCC
+DDD
+"""
+#with open('test.txt', 'w') as f:
+#	f.write(s)
+with open('test.txt', 'r') as f:
+	#print(f.read())
+	while True:
+		chunk=2
+		line = f.read(chunk)
+		print(line)
+		if not line:
+			break
 
 
 
--- run_watch.sh
+++ run_watch.sh
@@ -1,1 +1,1 @@
-watch -t -d "python main.py option1 option2 option3 > result.txt 2> error.txt; cat ./result.txt | tail -n 20; cat error.txt | cat test.txt"
+watch -t -d "python main.py option1 option2 option3 > result.txt 2> error.txt; cat ./result.txt | tail -n 20; cat error.txt; echo '----file_output----'; cat test.txt"
