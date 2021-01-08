4e6078e710d9ede39bbb19bd327e7411d3eeab99
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-09 01:13:35 +0900

    特殊メソッドを扱おう

--- main.py
+++ main.py
@@ -1606,6 +1606,39 @@ print(Person.kind)
 print(Person.what_is_your_kind())
 Person.about(1999)
 
+print("""\
+########################
+# 特殊メソッドを扱おう #
+########################\
+""")
+class Word(object):
+	def __init__(self, text):
+		self.text = text
+
+	def __str__(self):
+		return 'Word!!!!!'
+	
+	def __len__(self):
+		return len(self.text)
+
+	def __add__(self, word):
+		return self.text.lower() + word.text.lower()
+	
+	def __eq__(self,word):
+		return self.text.lower() == word.text.lower()
+
+w = Word('test')
+w2 = Word('test')
+print(w)
+print(len(w))
+print(w+w2)
+print(w==w2)
+
+
+
+
+
+
 
 
 
