811f00b8514daabcb97ad415fc3c69d8c6df4b68
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-10 06:58:30 +0900

    datetimeを扱おう

--- main.py
+++ main.py
@@ -1817,7 +1817,52 @@ print("""\
 #output=p2.communicate()[0]
 #print(output)
 
+print("""\
+####################
+# datetimeを扱おう #
+####################\
+""")
+import datetime
 
+now = datetime.datetime.now()
+#print(now)
+#print(now.isoformat())
+#print(now.strftime('%d/%m/%y-%H%M%S%f'))
+today=datetime.date.today()
+print(today)
+#print(today.isoformat())
+#print(today.strftime('%d/%m/%y'))
+#t=datetime.time(hour=1,minute=10,second=5,microsecond=100)
+#print(t)
+#print(t.isoformat())
+#print(t.strftime('%H_%M_%S_%f'))
+
+#print(now)
+d=datetime.timedelta(weeks=3)
+d=datetime.timedelta(days=3)
+d=datetime.timedelta(hours=3)
+d=datetime.timedelta(minutes=3)
+d=datetime.timedelta(seconds=3)
+d=datetime.timedelta(microseconds=3)
+#print(now-d)
+
+import time
+print('###')
+##time.sleep(2)
+print('###')
+
+#print(time.time())
+
+import os
+import shutil
+
+#file_name='test.txt'
+#if os.path.exists(file_name):
+#	shutil.copy(file_name, "{}.{}".format(
+#		file_name, now.strftime('%Y_%m_%d_%H_%M_%S')))
+
+#with open(file_name, 'w') as f:
+#	f.write('test')
 
 
 
